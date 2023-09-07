1527acafe1cba95805cbee28d82535053cc41364
y <hui1.teng.lim@intel.com>
2023-09-07 09:10:54 +0800

    Enabled the pin assignment by quartus
    Signed-off-by: y <hui1.teng.lim@intel.com>

--- sm_soc_devkit_ghrd/create_ghrd_quartus.tcl
+++ sm_soc_devkit_ghrd/create_ghrd_quartus.tcl
@@ -185,55 +185,54 @@ if {[info exists pin_assignment_table]} {
 
 set_instance_assignment -name IO_STANDARD "1.8 V" -to hps_osc_clk
 
-if {$hps_emif_en} {
-   if {$board  == "hidden"} {
-   
-#   set ranks r1
-#   set width $hps_emif_width
-   set ecc   $hps_emif_ecc_en
-   set key   x32_r1
-   }
-   if {$ecc} {
-      incr width 8
-   }
-
-#   set key "x${width}_$ranks"
-
-   # Search for key in the first line
-   set key_line [lindex $pin_matrix 0]
-   set idx [lsearch $key_line $key]
-   
-   if {$idx < 0} {
-      error "Could not locate configuration $key for EMIF generation"
-   }
-
-   set mem_type_idx [lsearch $key_line "MEM"]
-
-   if {$mem_type_idx < 0} {
-      error "Could not locate memory type specifier in pinout matrix for EMIF generation"
-   }
-
-   puts "key = $key"
-   puts "board = $board"
-   puts "mem_type = $hps_emif_type"
-
-   # Now add all items
-   set skip_first 1
-   foreach key_line $pin_matrix {
-      if {$skip_first} {
-         set skip_first 0
-      } else {
-         set pin [lindex $key_line $idx]
-         set mem_type [lindex $key_line $mem_type_idx]
-
-         if {$pin != "unused" && (($mem_type == $hps_emif_type) || ($mem_type == "both"))} {
-            set_location_assignment $pin -to [lindex $key_line 0]
-            puts "Setting: set_location_assignment $pin -to [lindex $key_line 0]"
-         }
-      }
-   }
-}
-set_instance_assignment -name IO_STANDARD "1.8 V" -to hps_osc_clk
+#if {$hps_emif_en} {
+#   if {$board  == "hidden"} {
+#   
+##   set ranks r1
+##   set width $hps_emif_width
+#   set ecc   $hps_emif_ecc_en
+#   set key   x32_r1
+#   }
+#   if {$ecc} {
+#      incr width 8
+#   }
+#
+##   set key "x${width}_$ranks"
+#
+#   # Search for key in the first line
+#   set key_line [lindex $pin_matrix 0]
+#   set idx [lsearch $key_line $key]
+#   
+#   if {$idx < 0} {
+#      error "Could not locate configuration $key for EMIF generation"
+#   }
+#
+#   set mem_type_idx [lsearch $key_line "MEM"]
+#
+#   if {$mem_type_idx < 0} {
+#      error "Could not locate memory type specifier in pinout matrix for EMIF generation"
+#   }
+#
+#   puts "key = $key"
+#   puts "board = $board"
+#   puts "mem_type = $hps_emif_type"
+#
+#   # Now add all items
+#   set skip_first 1
+#   foreach key_line $pin_matrix {
+#      if {$skip_first} {
+#         set skip_first 0
+#      } else {
+#         set pin [lindex $key_line $idx]
+#         set mem_type [lindex $key_line $mem_type_idx]
+#
+#         if {$pin != "unused" && (($mem_type == $hps_emif_type) || ($mem_type == "both"))} {
+#            set_location_assignment $pin -to [lindex $key_line 0]
+#            puts "Setting: set_location_assignment $pin -to [lindex $key_line 0]"
+#         }
+#      }
+#   }
+#}
 
 if {$hps_io_off == 0} {
 set_instance_assignment -name IO_STANDARD "1.8 V" -to hps_jtag_tck
