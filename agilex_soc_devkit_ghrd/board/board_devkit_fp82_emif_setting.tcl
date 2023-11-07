1893418afe4e2682de13019baee07d89a74d55d8
Wong, Felix Siew An <felix.siew.an.wong@intel.com>
2023-11-07 14:40:18 +0800

    Update emif IP parameter due to version 23.3 to 23.4
    Signed-off-by: Wong, Felix Siew An <felix.siew.an.wong@intel.com>

--- agilex_soc_devkit_ghrd/board/board_devkit_fp82_emif_setting.tcl
+++ agilex_soc_devkit_ghrd/board/board_devkit_fp82_emif_setting.tcl
@@ -37,6 +37,16 @@
 			set_component_param	"emif_hps PHY_AC_PLACEMENT PHY_AC_PLACEMENT_BOT"
 			set_component_param	"emif_hps PHY_REFCLK_FREQ_MHZ_AUTO_BOOL 0"
 			set_component_param	"emif_hps PHY_REFCLK_FREQ_MHZ 100.0"
+			set_component_param	"emif_hps GRP_PHY_DATA_AUTO_BOOL 0"
+			set_component_param "emif_hps GRP_PHY_DATA_X_R_T_DQ_INPUT_OHM {RTT_PHY_IN_60_CAL}"
+			set_component_param "emif_hps GRP_PHY_DATA_X_DQ_VREF 75.0"
+			set_component_param "emif_hps GRP_MEM_ODT_DQ_AUTO_BOOL 0"
+			set_component_param "emif_hps GRP_MEM_ODT_DQ_X_TGT_WR {MEM_RTT_COMM_OFF}"
+			set_component_param "emif_hps GRP_MEM_DQ_VREF_AUTO_BOOL 0"
+			set_component_param "emif_hps GRP_MEM_DQ_VREF_X_VALUE 75.0"
+			set_component_param "emif_hps GRP_MEM_VREF_CA_AUTO_BOOL 0"
+			set_component_param "emif_hps GRP_MEM_VREF_CA_X_CA_VALUE 75.0"
+			set_component_param "emif_hps GRP_MEM_VREF_CA_X_CS_VALUE 75.0"
 			set_component_param	"emif_hps USER_EXTRA_PARAMETERS BYTE_SWIZZLE_CH0=1,0,X,X,2,3,X,X;PIN_SWIZZLE_CH0_DQS1=14,12,8,10,15,11,9,13;PIN_SWIZZLE_CH0_DQS0=0,6,2,4,1,7,3,5;PIN_SWIZZLE_CH0_DQS2=22,16,18,20,19,23,17,21;PIN_SWIZZLE_CH0_DQS3=30,24,26,28,31,27,29,25;"
 			
         }
