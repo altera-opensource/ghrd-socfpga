1fa8e899a316e399c40bcd43eb2b82efc7920622
Ong, Lean Kim <lean.kim.ong@intel.com>
2023-11-15 11:13:27 +0800

    Ammend SDMMC quadrant of AIC0 pinmux assignment
    Signed-off-by: Ong, Lean Kim <lean.kim.ong@intel.com>

--- sm_soc_devkit_ghrd/hps_subsys/agilex_io48.tcl
+++ sm_soc_devkit_ghrd/hps_subsys/agilex_io48.tcl
@@ -83,7 +83,7 @@ set hps_i3c1_q3_en           0
 set hps_i3c1_q4_en           0
 set hps_nand_q12_en          0
 set hps_nand_q34_en          0
-set hps_emmc_q34_en 		 0
+set hps_emmc_q34_en          0
 set hps_sdmmc4b_q1_en        0
 set hps_sdmmc8b_q1_en        0
 set hps_sdmmc_wp_q1_en       0
@@ -173,7 +173,7 @@ set hps_cm_io 2
 set hps_trace_q34_en 1
 set hps_trace_8b_en 1
 
-} elseif {$daughter_card == "tsn_phy_aic0"} {
+} elseif {$daughter_card == "tsn_aic0"} {
 set hps_emac0_q1_en 1
 set hps_uart0_q1_en 1
 set hps_emac2_q1_en 1
@@ -182,13 +182,13 @@ set hps_cm_q 1
 set hps_cm_io 10
 set hps_mdio0_q1_en 1
 set hps_emac0_rgmii_en 1
-set hps_sdmmc4b_q1_en 1
+set hps_sdmmc4b_q3_en 1
 #set hps_sdmmc4b_q1_sel_en 1
 #set hps_sdmmc4b_q1_alt_en 1
 set hps_jtag_en 1
 set hps_emac2_rgmii_en 1
 
-} elseif {$daughter_card == "tsn_phy_aic2"} {
+} elseif {$daughter_card == "tsn_aic2"} {
 set hps_emac0_q1_en 1
 set hps_emac1_q1_en 1
 set hps_emac2_q1_en 1
