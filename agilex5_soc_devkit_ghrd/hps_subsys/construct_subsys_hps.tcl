95c95b2d78a8a3f7059ac43264e73d2d570a58a2
Wong, Felix Siew An <felix.siew.an.wong@intel.com>
2024-07-22 15:41:02 +0800

    #15016494470 Agilex5_soc_devkit_ghrd: Remove GHRD f2s_mode parameter since SM HPS QHIP F2H only support ace5-lite mode and use new ace5-lite qsys interface type.
    Signed-off-by: Wong, Felix Siew An <felix.siew.an.wong@intel.com>

--- agilex5_soc_devkit_ghrd/hps_subsys/construct_subsys_hps.tcl
+++ agilex5_soc_devkit_ghrd/hps_subsys/construct_subsys_hps.tcl
@@ -80,7 +80,6 @@ add_component_param "intel_agilex_5_soc agilex_hps
 					 f2sdram_address_width $f2sdram_addr_width
 					 f2s_data_width $f2s_data_width
 					 f2s_address_width $f2s_address_width
-					 f2s_mode axi4
                      LWH2F_Width $lwh2f_width
 					 LWH2F_Address_Width $lwh2f_addr_width
                      EMIF_AXI_Enable $hps_emif_en
